d005168e40889bf160574ed75fa7c9ce423f8680
yingrui <yingrui.f@gmail.com>
2016-10-24 14:18:35 +0800

    @yingrui: implements an example to sent http get method with certificate authentication

--- src/test/java/me/yingrui/learning/bidirection/ssl/GreetingControllerTest.java
+++ src/test/java/me/yingrui/learning/bidirection/ssl/GreetingControllerTest.java
@@ -1,13 +1,5 @@
 package me.yingrui.learning.bidirection.ssl;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.authentication.CertificateAuthSettings;
-import com.jayway.restassured.config.SSLConfig;
-import com.jayway.restassured.response.Response;
-import com.jayway.restassured.response.ResponseBody;
-import org.apache.http.HttpStatus;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,12 +8,23 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.util.Properties;
 
-import static com.jayway.restassured.RestAssured.expect;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * http://www.rap.ucar.edu/staff/paddy/cacerts
+ * http://www.rap.ucar.edu/staff/paddy/cacerts/index.html
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
@@ -33,35 +36,63 @@ public class GreetingControllerTest {
 
     String password = "changeit";
 
-    @Before
-    public void onSetUp() {
-        RestAssured.useRelaxedHTTPSValidation();
-        RestAssured.port = port;
-    }
-
     @Test
     public void should_connect_to_server_and_get_response() throws IOException {
-        SSLConfig sslConfig = new SSLConfig()
-                .trustStore("/Users/twer/workspace/learning-2ways-authentication/keystore/truststore.jks", "changeit")
-                .keystore("/Users/twer/workspace/learning-2ways-authentication/keystore/keystore.jks", "changeit")
-                .allowAllHostnames().relaxedHTTPSValidation();
+        Properties systemProps = System.getProperties();
+        systemProps.put("javax.net.ssl.trustStore", "./keystore/keystore.jks");
+        systemProps.put("javax.net.ssl.trustStorePassword", password);
+        System.setProperties(systemProps);
 
-        CertificateAuthSettings pkcs12 = new CertificateAuthSettings().
-                trustStore(sslConfig.getTrustStore()).
-                trustStoreType("PKCS12");
+        try {
+            // Open a secure connection.
+            URL url = new URL(String.format("https://localhost:%d/hello", port));
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
 
-        Response response = expect()
-                .statusCode(HttpStatus.SC_OK)
-                .given()
-//                .config(RestAssured.config().sslConfig(sslConfig))
-                .auth().certificate("/Users/twer/workspace/learning-2ways-authentication/keystore/cid.p12", "changeit", pkcs12)
-                .when().get(String.format("https://localhost:%d/hello", port));
+            // Set up the connection properties
+            con.setRequestProperty("Connection", "close");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+            con.setConnectTimeout(30000);
+            con.setReadTimeout(30000);
+            con.setRequestMethod("GET");
 
-        System.out.println(response.getStatusLine());
-        System.out.println(response.getHeaders().toString());
-        ResponseBody body = response.getBody();
-        body.print();
-        Assert.assertEquals("hello world", body.asString());
+            File pKeyFile = new File("keystore/cid.p12");
+            String pKeyPassword = password;
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            InputStream keyInput = new FileInputStream(pKeyFile);
+            keyStore.load(keyInput, pKeyPassword.toCharArray());
+            keyInput.close();
+            keyManagerFactory.init(keyStore, pKeyPassword.toCharArray());
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
+            SSLSocketFactory sockFact = context.getSocketFactory();
+            con.setSSLSocketFactory(sockFact);
+
+            // Check for errors
+            int responseCode = con.getResponseCode();
+            InputStream inputStream;
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                inputStream = con.getInputStream();
+            } else {
+                inputStream = con.getErrorStream();
+            }
+
+            // Process the response
+            BufferedReader reader;
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                sb.append(line);
+            }
+            assertEquals("hello world", sb.toString());
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
